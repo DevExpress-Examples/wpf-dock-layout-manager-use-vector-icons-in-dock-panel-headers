81cf46f95e58b299dc9bbf7c956496b55be90130
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/XAMLIcons/MainWindow.xaml.vb
@@ -0,0 +1,12 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.Xpf.Core
+
+Namespace XAMLIcons
+	Partial Public Class MainWindow
+		Inherits DXWindow
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
